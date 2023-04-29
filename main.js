4677a8acf78710a0d40d8d143b85636cee86d1f6
Altenia Gaines <gaines.tina90@gmail.com>
2023-04-28 17:49:39 -0700

    complet lab

--- main.js
+++ main.js
@@ -3,6 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.querySelector('#modal')
+modal.className = ('hidden');
+
+
+
+document.querySelectorAll('.like-glyph').forEach((heart) => {
+  heart.addEventListener("click", (e) => {
+    mimicServerCall()
+      .then(() => changeHeart(e))
+      .catch(handleError);
+  });
+});
+
+
+// Handler Functions
+function changeHeart(event) {
+  const liker = event.target;
+  if (liker.textContent === EMPTY_HEART) {
+    liker.textContent = FULL_HEART;
+    liker.classList.add("activated-heart");
+  } else if ((liker.textContent = FULL_HEART)) {
+    liker.textContent = EMPTY_HEART;
+    liker.classList.remove("activated-heart");
+  }
+}
+function handleError(message) {
+  errorModal.className.remove("hidden");
+  document.getElementById("modal-message").textContent = message;
+  setTimeout(() => errorModal.className.add("hidden"), 3000);
+}
 
 
 
